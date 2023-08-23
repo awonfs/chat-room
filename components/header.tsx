9ccb26e84a54526408a7b98fd93ef8b8aaaff655
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-08-23 21:55:12 +0300

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="text-center w-full py-6">
+      <h1 className="text-bold text-2xl">ChatRoom.io</h1>
+    </header>
+  );
+}
+
+export default Header;
